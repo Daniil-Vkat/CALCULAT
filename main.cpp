b9952e8d082847c527ed90fad1e6156fc2a38580
Daniil-Vkat <Dragafly@mail.ru>
2026-09-12 10:01:16 +0000

    добавил CMakelist,добавил проверку на переполнение

--- main.cpp
+++ main.cpp
@@ -1,111 +1,166 @@
 #include <iostream>
-
-int addition(int a, int b)
-{
-	return a + b;
-}
-
-int subtraction(int a, int b)
-{
-	return a - b;
-}
-
-int multiplication(int a, int b)
-{
-	return a * b;
-}
-
-int division(int a, int b, int& c)
-{
-	if (b == 0)
-	{
-		return -1;
-	}
-	c = a / b;
-	return 0;
-}
-
-int power(int a, int b)
-{
-	int c = 1;
-	for (int i = 0; i < b; ++i)
-	{
-		c = c * a;
-	}
-	return c;
-}
-
-struct Task
-{
-	int value1;
-	char operation;
-	int value2;
-	int status;
-	int result;
+#include <string>
+#include <limits>
+ 
+// Статус вместо магических чисел
+enum class Status {
+    Ok,
+    DivisionByZero,
+    UnknownOperation,
+    BadArgumentCount,
+    InvalidNumber,
+    Overflow,
+    NegativeExponent,
+    NegativeFactorial
 };
-
-void parse(int argc, char** argv, Task& task)
-{
-	// TODO Добавить проверку, что argc равно 4
-	task.value1 = std::atoi(argv[1]);
-	task.operation = *(argv[2]);
-	task.value2 = std::atoi(argv[3]);
+ 
+struct Task {
+    long long value1 = 0;
+    char operation = '\0';
+    long long value2 = 0;
+    Status status = Status::Ok;
+    long long result = 0;
+};
+ 
+ 
+// ---------- Операции ----------
+ 
+Status addition(long long a, long long b, long long& out) {
+    long long r;
+    if (__builtin_add_overflow(a, b, &r)) return Status::Overflow;
+    out = r;
+    return Status::Ok;
 }
-
-void calculate(Task& task)
-{
-	task.status = 0;
-	switch(task.operation)
-	{
-		case '+':
-			task.result = addition(task.value1, task.value2);
-			break;
-		case '-':
-			task.result = subtraction(task.value1, task.value2);
-			break;
-		case '*':
-			task.result = multiplication(task.value1, task.value2);
-			break;
-		case '/':
-			task.status = division(task.value1, task.value2, task.result);
-			break;
-		case '^':
-			task.result = power(task.value1, task.value2);
-			break;
-		default:
-			task.status = 1;
-	}
+ 
+Status subtraction(long long a, long long b, long long& out) {
+    long long r;
+    if (__builtin_sub_overflow(a, b, &r)) return Status::Overflow;
+    out = r;
+    return Status::Ok;
 }
-
-void output(Task task)
-{
-	if (task.status == 0)
-	{
-		std::cout << task.value1 << ' ' << task.operation << ' ' << task.value2 << " = " << task.result << '\n';
-	}
-	else if (task.status == -1)
-	{
-		std::cout << "Error! Division by zero!\n";
-	}
-	else if (task.status == 1)
-	{
-		std::cout << "Error! Unknown operation!\n";
-	}
-	else
-	{
-		std::cout << "Unknown error\n";
-	}
+ 
+Status multiplication(long long a, long long b, long long& out) {
+    long long r;
+    if (__builtin_mul_overflow(a, b, &r)) return Status::Overflow;
+    out = r;
+    return Status::Ok;
 }
-
-void run(int argc, char** argv)
-{
-	Task task;
-	parse(argc, argv, task);
-	calculate(task);
-	output(task);
+ 
+Status division(long long a, long long b, long long& out) {
+    if (b == 0) return Status::DivisionByZero;
+    if (a == std::numeric_limits<long long>::min() && b == -1) return Status::Overflow;
+    out = a / b;
+    return Status::Ok;
 }
-
-int main(int argc, char** argv)
-{
-	run(argc, argv);
+ 
+Status power(long long a, long long b, long long& out) {
+    if (b < 0) return Status::NegativeExponent;
+    long long c = 1;
+    for (long long i = 0; i < b; ++i) {
+        if (__builtin_mul_overflow(c, a, &c)) return Status::Overflow;
+    }
+    out = c;
+    return Status::Ok;
+}
+ 
+Status factorial(long long n, long long& out) {
+    if (n < 0) return Status::NegativeFactorial;
+    long long r = 1;
+    for (long long i = 2; i <= n; ++i) {
+        if (__builtin_mul_overflow(r, i, &r)) return Status::Overflow;
+    }
+    out = r;
+    return Status::Ok;
+}
+ 
+// ---------- Парсинг ----------
+ 
+bool parseNumber(const std::string& s, long long& out) {
+    try {
+        size_t pos = 0;
+        long long v = std::stoll(s, &pos);
+        if (pos != s.size()) return false;
+        out = v;
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+ 
+bool parse(int argc, char** argv, Task& task) {
+    if (argc != 4) {
+        task.status = Status::BadArgumentCount;
+        return false;
+    }
+    if (!parseNumber(argv[1], task.value1)) {
+        task.status = Status::InvalidNumber;
+        return false;
+    }
+    if (argv[2][0] == '\0') {
+        task.status = Status::UnknownOperation;
+        return false;
+    }
+    task.operation = argv[2][0];
+    if (!parseNumber(argv[3], task.value2)) {
+        task.status = Status::InvalidNumber;
+        return false;
+    }
+    return true;
+}
+ 
+// ---------- Вычисление ----------
+ 
+void calculate(Task& task) {
+    switch (task.operation) {
+        case '+': task.status = addition(task.value1, task.value2, task.result); break;
+        case '-': task.status = subtraction(task.value1, task.value2, task.result); break;
+        case '*': task.status = multiplication(task.value1, task.value2, task.result); break;
+        case '/': task.status = division(task.value1, task.value2, task.result); break;
+        case '^': task.status = power(task.value1, task.value2, task.result); break;
+        case '!': task.status = factorial(task.value1, task.result); break;
+        default: task.status = Status::UnknownOperation;
+    }
+}
+ 
+// ---------- Вывод ----------
+ 
+void output(const Task& task) {
+    switch (task.status) {
+        case Status::Ok:
+            if (task.operation == '!')
+                std::cout << task.value1 << "! = " << task.result << '\n';
+            else
+                std::cout << task.value1 << ' ' << task.operation << ' '
+                          << task.value2 << " = " << task.result << '\n';
+            break;
+        case Status::DivisionByZero:
+            std::cout << "Error! Division by zero!\n"; break;
+        case Status::UnknownOperation:
+            std::cout << "Error! Unknown operation!\n"; break;
+        case Status::BadArgumentCount:
+            std::cout << "Error! Usage: <num1> <op> <num2>\n"; break;
+        case Status::InvalidNumber:
+            std::cout << "Error! Invalid number!\n"; break;
+        case Status::Overflow:
+            std::cout << "Error! Overflow!\n"; break;
+        case Status::NegativeExponent:
+            std::cout << "Error! Negative exponent is not supported!\n"; break;
+        case Status::NegativeFactorial:
+            std::cout << "Error! Factorial of a negative number is undefined!\n"; break;
+    }
+}
+ 
+// ---------- Запуск ----------
+ 
+void run(int argc, char** argv) {
+    Task task;
+    if (parse(argc, argv, task)) {
+        calculate(task);
+    }
+    output(task);
+}
+ 
+int main(int argc, char** argv) {
+    run(argc, argv);
+    return 0;
 }
